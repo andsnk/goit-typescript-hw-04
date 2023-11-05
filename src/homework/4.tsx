49ab02e209d93c87b346adb3faacd694cce8b68f
andsnk <127045868+andsnk@users.noreply.github.com>
2023-11-06 00:38:33 +0200

    finish 3 tasks

--- src/homework/4.tsx
+++ src/homework/4.tsx
@@ -1,3 +1,18 @@
+/*
+Ви вирішили застосувати до меню контекст і тепер вам потрібно його типізувати.
+
+Описати тип SelectedMenu: Це має бути об'єкт, який містить id з типом MenuIds
+
+Описати тип MenuSelected: Цей тип є об'єктом, що містить selectedMenu
+
+Описати тип MenuAction: Цей тип являє собою об'єкт з методом onSelectedMenu, який приймає об'єкт типу SelectedMenu 
+як аргумент повертає void.
+
+Описати тип PropsProvider: Опишіть правильний тип для дітей
+
+Описати тип PropsMenu: Опишіть тип для menus, він має бути від типу Menu
+*/
+
 import React, { createContext, useMemo, useState, useContext } from "react";
 import noop from "lodash/noop";
 
@@ -6,11 +21,22 @@ type Menu = { id: MenuIds; title: string };
 
 // Додати тип Menu Selected
 
+type SelectedMenu = {
+  id: MenuIds;
+}
+
+type MenuSelected = {
+  selectedMenu: SelectedMenu;
+}
+
 const MenuSelectedContext = createContext<MenuSelected>({
-  selectedMenu: {},
+  selectedMenu: { id: 'first' },
 });
 
 // Додайте тип MenuAction
+type MenuAction = {
+  onSelected
+}
 
 const MenuActionContext = createContext<MenuAction>({
   onSelectedMenu: noop,
